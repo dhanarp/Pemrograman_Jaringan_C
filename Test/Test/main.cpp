e0eae025a802209abb785efbe14d87d6db089b70
Dhanar Prayoga <dhanarp918@gmail.com>
2015-10-02 03:07:47 +0700

    Tes Commit 2
    Apus file test.c, buat project baru, added main.cpp

---
+++ Test/Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test
+//
+//  Created by dhanar prayoga on 10/2/15.
+//  Copyright (c) 2015 dhanar prayoga. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
